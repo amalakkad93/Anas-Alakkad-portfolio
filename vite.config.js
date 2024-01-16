8f1e2664a7e205bf080fcac3992f9d7a0088b996
amala <amalakkad93@outlook.com>
2024-01-16 05:27:54 -0800

    first commit2

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+// export default defineConfig({
+//   base: '/',
+//   plugins: [react()],
+// });
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
+  base: '/',
   plugins: [react()],
-    build: {
-        chunkSizeWarningLimit: 100000000
-    },
-    base: "/3d-react-portfolio",
-})
+  build: {
+    outDir: 'build',
+    chunkSizeWarningLimit: 100000000,
+  },
+});
